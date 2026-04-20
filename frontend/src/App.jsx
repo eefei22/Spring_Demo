db2c7acc510522956256a1b03f2da48ba6e3e41b
eefei <loweefei88@gmail.com>
2026-04-20 11:01:08 +0800

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import LoginPage from './pages/LoginPage.jsx'
+import JournalsPage from './pages/JournalsPage.jsx'
+import TokenStorage from './utils/TokenStorage.js'
+
+const VIEW = {
+  login: 'login',
+  journals: 'journals',
+}
+
+export default function App() {
+  const [view, setView] = useState(() =>
+    TokenStorage.getToken() ? VIEW.journals : VIEW.login,
+  )
+
+  function handleLoginSuccess() {
+    setView(VIEW.journals)
+  }
+
+  function handleLogout() {
+    TokenStorage.clearToken()
+    setView(VIEW.login)
+  }
+
+  if (view === VIEW.journals) {
+    return <JournalsPage onLogout={handleLogout} />
+  }
+
+  return <LoginPage onLoginSuccess={handleLoginSuccess} />
+}
+
+
+
+
+
+
+/**
+  ===    strict equality
+  !==    strict not equal
+  ? :    inline if/else
+  ?.     safe property access
+  ${}    insert variable into string
+  ??     fallback if null/undefined
+ 
+ **/
